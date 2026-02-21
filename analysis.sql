ef384b6e2260fa29f94ba0781b2af9b58be63c55
SeunJegede <jegeseun@gmail.com>
2026-02-21 16:59:18 +0000

    Update data cleaning file with fact table query

--- analysis.sql
+++ analysis.sql
@@ -1,1 +1,7 @@
-SELECT * FROM order_items;
+WITH customer_summary AS(
+    SELECT
+    user_id,
+    DATE_DIFF()
+    FROM
+    retail_fact_table
+    )
--- data_cleaning.sql
+++ data_cleaning.sql
@@ -10,20 +10,44 @@ SELECT id, order_id, status
 FROM order_items
 WHERE status = 'Complete';
 
--- create the fact table that informs analysis
+
+--Applying VAT for the UK as 20%
+-- Net sales = Gross sales / 1.2
+-- Net sales show the profit made from orders after tax
+
+SELECT order_id, ROUND(sale_price / 1.2 , 2) AS _net_sales_price_rounded
+FROM order_items
+GROUP BY order_id, sale_price
+ORDER BY order_id;
+
+-- The fact table
+-- Data is cleaned to show net sales for each complete order in the UK states/region
+-- Join the user, order and order items tables
+-- Excluded non-completed orders
+-- Calculate net revenue for each order
+
 WITH retail_fact_table AS (
-    SELECT
+SELECT
     oi.order_id,
     oi.user_id,
     oi.product_id,
+    u.state,
     DATE(o.created_at) AS created_date,
-    ROUND(p.retail_price * o.num_of_item, 2) AS Sales_Price
+    ROUND(oi.sale_price / 1.2, 2) as net_sales_rounded
     FROM order_items oi
     INNER JOIN orders o
-		ON oi.order_id = o.order_id
-	INNER JOIN products p
-		ON p.id = oi.product_id
-    WHERE o.status = 'Complete'
-	)
+    ON oi.order_id = o.order_id
+    INNER JOIN users u
+    ON oi.user_id = u.id
+WHERE o.status = 'Complete'
+AND u.country = 'United Kingdom'
+GROUP BY 
+    oi.order_id,
+    oi.user_id,
+    oi.product_id,
+    u.state,
+    DATE(o.created_at),
+    oi.sale_price
+  )
 SELECT *
 FROM retail_fact_table;
